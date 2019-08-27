6b7d4ac34e2aa48d4175cd62ef6f08473caf2080
Andrew Stiles <ndrwstls544@gmail.com>
2019-08-26 23:13:06 -0400

    finished 1st part

---
+++ bamazon.sql
@@ -0,0 +1,43 @@
+drop database if exists bamazon;
+
+create database bamazon;
+
+use bamazon;
+
+create table products (
+    item_id int(10) not null,
+    product_name varchar(30) not null,
+    department_name varchar(30) not null,
+    price decimal(10, 3) not null,
+    stock_quantity int(20) not null 
+);
+
+insert into products (item_id, product_name, department_name, price, stock_quantity)
+values (1, 'cordless vacuum', 'household items', 190.99, 80);
+
+insert into products (item_id, product_name, department_name, price, stock_quantity)
+values (2, 'Mario Kart 25', 'video games', 49.99, 30);
+
+insert into products (item_id, product_name, department_name, price, stock_quantity)
+values (3, 'couch', 'household items', 399.99, 8);
+
+insert into products (item_id, product_name, department_name, price, stock_quantity)
+values (4, 'led flashing bluetooth speaker', 'electronics', 25.99, 130);
+
+insert into products (item_id, product_name, department_name, price, stock_quantity)
+values (5, 'iphone 20', 'electronics', 1499.99, 40);
+
+insert into products (item_id, product_name, department_name, price, stock_quantity)
+values (6, 'polarized sunglasses', 'fashion', 19.99, 200);
+
+insert into products (item_id, product_name, department_name, price, stock_quantity)
+values (7, 'orange juice', 'food and drinks', 3.99, 400);
+
+insert into products (item_id, product_name, department_name, price, stock_quantity)
+values (8, 'himalayan salt lamp', 'home decor', 19.99, 600);
+
+insert into products (item_id, product_name, department_name, price, stock_quantity)
+values (9, '8k TV', 'electronics', 7999.99, 4);
+
+insert into products (item_id, product_name, department_name, price, stock_quantity)
+values (10, 'led nighthawk drone', 'electronics', 1200, 13);
---
+++ bamazonCustomer.js
@@ -0,0 +1,90 @@
+let mysql = require('mysql');
+let inquirer = require('inquirer');
+
+let connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Butterfly13!",
+    database: "bamazon"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    displayItems()
+});
+
+function displayItems() {
+    console.log("Selecting all products...\n");
+    connection.query('SELECT * FROM products', function(err, results) {
+        if (err) throw err;
+        for (let i = 0; i < results.length; i++) {
+            console.log(results[i].product_name);
+        }
+    })
+    selectItem()
+}
+
+function selectItem() {
+    connection.query('SELECT * FROM products', function(err, results) {
+        if (err) throw err;
+        inquirer
+            .prompt([
+                {
+                    name: 'id',
+                    type: 'list',
+                    choices: function() {
+                        var choiceArray = [];
+                        for (var i = 0; i < results.length; i++) {
+                          choiceArray.push(results[i].item_id);
+                        }
+                        return choiceArray;
+                    },
+                    message: 'What is the ID of the product you would like to buy?',
+                },
+                {
+                    type: 'input',
+                    message: 'How many would you like to purchase?',
+                    name: 'units'
+                }
+            ]).then(function(answer) {
+                for (var i = 0; i < results.length; i++) {
+                    if (results[i].item_id === answer.id) {
+                        var chosenItem = results[i];
+                        var quantity = chosenItem.stock_quantity
+                        var price = chosenItem.price
+                        var name = chosenItem.product_name
+                        console.log(chosenItem)
+                    }
+                }
+                
+                if (quantity >= answer.units) {
+                    let units = answer.units
+                    fulfillOrder(name, quantity, units, price)
+                }
+                else {
+                    console.log('insufficient quantity!')
+                    connection.end()
+                }
+            })
+    })
+}
+
+function fulfillOrder(name, quantity, units, price) {   
+    connection.query(
+        "UPDATE products SET ? WHERE ?",
+        [
+            {
+                stock_quantity: quantity -= units
+            },
+            {
+                product_name: name
+            }
+        ],
+    );
+        console.log(quantity)
+        console.log('congratulations your order was received successfully' + ', ' +  'Your total cost is: ' + '$' + price * units)     
+        connection.end()
+}
+
